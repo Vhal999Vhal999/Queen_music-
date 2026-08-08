de7c9777cd341d595661996089ae1345074ef1e1
Cookies Are On Github <107368993+Vhal999Vhal999@users.noreply.github.com>
2026-08-08 17:36:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ class Config:
         self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
         self.OWNER_ID = int(getenv("OWNER_ID", 0))
 
-        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60)) * 60
-        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
-        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 20))
+        self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 18000)) * 60
+        self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 30))
+        self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 30))
 
         self.SESSION1 = getenv("SESSION", None)
         self.SESSION2 = getenv("SESSION2", None)
