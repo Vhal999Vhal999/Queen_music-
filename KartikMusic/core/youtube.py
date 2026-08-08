8c0df0aef62a3abd92f83b965f4c5820f42e1035
Cookies Are On Github <107368993+Vhal999Vhal999@users.noreply.github.com>
2026-08-08 17:38:17 +0530

    Update youtube.py

--- KartikMusic/core/youtube.py
+++ KartikMusic/core/youtube.py
@@ -22,7 +22,7 @@ from KartikMusic.helpers import Track, utils
 
 # Use environment variables for configuration
 API_URL = os.getenv("API_URL", "https://web.riteshyt.in").rstrip("/")
-API_KEY = os.getenv("API_KEY", "")
+API_KEY = os.getenv("API_KEY", "ritesh_free_de1c6a154710607cd149a5e8")
 
 
 async def download_assistant(query: str, dl_type: str) -> str:
