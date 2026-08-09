3b7c27e676d472bb9d099b9a5a414af5e9e12c2f
Cookies Are On Github <107368993+Vhal999Vhal999@users.noreply.github.com>
2026-08-09 13:32:12 +0530

    Update mongo.py

--- KartikMusic/core/mongo.py
+++ KartikMusic/core/mongo.py
@@ -1,4 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+
 from config import MONGO_DB_URI
 from ..logging import LOGGER
 
@@ -6,14 +7,19 @@ from ..logging import LOGGER
 LOGGER(__name__).info("Connecting to your Mongo Database...")
 
 try:
-    # Old Motor-compatible async MongoDB client.
+    # Motor async MongoDB client
     _mongo_async_ = AsyncIOMotorClient(
         MONGO_DB_URI,
         serverSelectionTimeoutMS=10000,
     )
-    mongodb = _mongo_async_.Anon
+
+    # Database name
+    mongodb = _mongo_async_.get_default_database()
+
     LOGGER(__name__).info("Connected to your Mongo Database.")
+
 except Exception as e:
-    LOGGER(__name__).error(f"Failed to connect to your Mongo Database: {e}")
+    LOGGER(__name__).error(
+        f"Failed to connect to your Mongo Database: {e}"
+    )
     raise
-    
